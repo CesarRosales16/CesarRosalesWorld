42c178d8929e065f3493de65a59af977136c49ce
Cesar Rosales <Cesar Rosales@192.168.1.6>
2018-05-30 21:48:09 -0600

    Main base (vacio) para desarrollo del juego

---
+++ src/cesarrosalesworld/CesarRosalesWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cesarrosalesworld;
+
+/**
+ *
+ * @author Cesar Rosales <00060917@uca.edu.sv>
+ */
+public class CesarRosalesWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
