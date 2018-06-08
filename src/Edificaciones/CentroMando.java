f4391bce22021007b2536d8f2495a81613764964
Cesar Rosales <Cesar Rosales@192.168.1.6>
2018-06-08 00:28:13 -0600

    Clase centro de mando con patron builder

---
+++ src/Edificaciones/CentroMando.java
@@ -0,0 +1,105 @@
+package Edificaciones;
+
+/**
+ *
+ * @author Cesar Rosales <00060917@uca.edu.sv>
+ */
+public class CentroMando {
+
+    private int vida;
+    private int nivel;
+    private int cantR1;
+    private int cantR2;
+    private int cantR3;
+    private int maxR1;
+    private int maxR2;
+    private int maxR3;
+
+    public CentroMando() {
+    }
+
+    private CentroMando(Builder b) {
+        this.vida = b.vida;
+        this.nivel = b.nivel;
+        this.cantR1 = b.cantR1;
+        this.cantR2 = b.cantR2;
+        this.cantR3 = b.cantR3;
+        this.maxR1 = b.maxR1;
+        this.maxR2 = b.maxR2;
+        this.maxR3 = b.maxR3;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public int getCantR1() {
+        return cantR1;
+    }
+
+    public int getCantR2() {
+        return cantR2;
+    }
+
+    public int getCantR3() {
+        return cantR3;
+    }
+
+    public int getMaxR1() {
+        return maxR1;
+    }
+
+    public int getMaxR2() {
+        return maxR2;
+    }
+
+    public int getMaxR3() {
+        return maxR3;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public static class Builder {
+
+        private int vida;
+        private int nivel;
+        private int cantR1;
+        private int cantR2;
+        private int cantR3;
+        private int maxR1;
+        private int maxR2;
+        private int maxR3;
+
+        public Builder(int vida, int nivel, int cantR1, int cantR2, int cantR3, int maxR1, int maxR2, int maxR3) {
+            this.vida = vida;
+            this.nivel = nivel;
+            this.cantR1 = cantR1;
+            this.cantR2 = cantR2;
+            this.cantR3 = cantR3;
+            this.maxR1 = maxR1;
+            this.maxR2 = maxR2;
+            this.maxR3 = maxR3;
+        }
+
+        public CentroMando build() {
+            return new CentroMando(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+                return "CentroMando{"
+                + ", vida='" + vida + '\''
+                + ", nivel='" + nivel + '\''
+                + ", cantR1='" + cantR1 + '\''
+                + ", cantR2='" + cantR2 + '\''
+                + ", cantR2='" + cantR2 + '\''
+                + ", maxR1='" + maxR1 + '\''
+                + ", maxR2='" + maxR2 + '\''
+                + ", maxR3='" + maxR3 + '\''
+                + '}';
+    }
+    
+}
