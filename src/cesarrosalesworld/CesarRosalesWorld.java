54f026f2a83a72f08ad7e588696dea525c8b5384
Cesar Rosales <Cesar Rosales@192.168.1.6>
2018-06-08 01:26:50 -0600

    Main medio funcional con creaciones de razas e interacion con menu

--- src/cesarrosalesworld/CesarRosalesWorld.java
+++ src/cesarrosalesworld/CesarRosalesWorld.java
@@ -1,14 +1,8 @@
 package cesarrosalesworld;
 
-import Edificaciones.CentroMando;
-import Edificaciones.DirectorEdificacion;
-import Edificaciones.Edificacion;
-import Milicia.DirectorMilicia;
-import Milicia.Milicia;
-import Recursos.DirectorRecurso;
-import Recursos.Recurso;
-import Vehiculos.DirectorVehiculo;
-import Vehiculos.Vehiculo;
+import Razas.Raza;
+import Razas.RazaFactory;
+import java.util.Scanner;
 
 /**
  *
@@ -20,22 +14,30 @@ public class CesarRosalesWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        DirectorVehiculo directorV = new DirectorVehiculo();
-        DirectorEdificacion directorE = new DirectorEdificacion();
-        DirectorRecurso directorR = new DirectorRecurso();
-        DirectorMilicia directorM = new DirectorMilicia();
-        Vehiculo v1 = directorV.getVehiculo("Tanque", 100, 13, 75, 35, 69);
-        Edificacion e1 = directorE.getEdificacion("Centro de Mando", 1000, 0, 0, 0, 0);
-        Recurso r1 = directorR.getRecurso("1", 1000, 1);
-        Milicia m1 = directorM.getMilicia("Soldado", 150, 20, 13, 50, 25, 7);
-        Milicia m2 = directorM.getMilicia("Boki", 330, 25, 15, 100, 75, 43, 8);
-        System.out.println(v1.toString());
-        System.out.println(e1.toString());
-        System.out.println(r1.toString());
-        System.out.println(m1.toString());
-        System.out.println(m2.toString());
+        int turno = 0, fase = 0;
+        String nombreJ1, nombreJ2;
+        Scanner input = new Scanner(System.in);
 
-        CentroMando cm1 = new CentroMando.Builder(1000, 1, 750, 750, 500, 10000, 5000, 3000).build();
-        System.out.println(cm1.toString());
+        System.out.print("Cesar Rosales's World ®\nBienvenido!\nIngrese el nombre del jugador 1: ");
+        nombreJ1 = input.next();
+        System.out.print("Ingrese el nombre del jugador 2: ");
+        nombreJ2 = input.next();
+        System.out.print("\nRazas disponibles:\n1. Los Sectarios\n2. La Resistencia\n3. La manada\nIngrese la raza que desea " + nombreJ1 + ": ");
+        int r1 = input.nextInt();
+        System.out.print("\nRazas disponibles:\n1. Los Sectarios\n2. La Resistencia\n3. La manada\nIngrese la raza que desea " + nombreJ2 + ": ");
+        int r2 = input.nextInt();
+
+        System.out.println("\nIniciando el juego");
+        Raza razaJ1 = RazaFactory.getRaza(r1);
+        razaJ1.crear();
+        Raza razaJ2 = RazaFactory.getRaza(r2);
+        razaJ2.crear();
+        Partida partida = new Partida();
+        while (fase < 10) {
+            System.out.println("Fase " + fase + ":");
+            turno = partida.turno(turno);
+            partida.fase(turno);
+            fase++;
+        }
     }
 }
