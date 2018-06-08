5d29d065bad5b1f4b090c87e9a0c7e3a8291d933
Cesar Rosales <Cesar Rosales@192.168.1.6>
2018-06-07 23:34:44 -0600

    Pruebas de creacion de vehiculos, edificaciones, milicia y recursos

--- src/cesarrosalesworld/CesarRosalesWorld.java
+++ src/cesarrosalesworld/CesarRosalesWorld.java
@@ -1,5 +1,11 @@
 package cesarrosalesworld;
 
+import Edificaciones.DirectorEdificacion;
+import Edificaciones.Edificacion;
+import Milicia.DirectorMilicia;
+import Milicia.Milicia;
+import Recursos.DirectorRecurso;
+import Recursos.Recurso;
 import Vehiculos.DirectorVehiculo;
 import Vehiculos.Vehiculo;
 
@@ -14,9 +20,19 @@ public class CesarRosalesWorld {
      */
     public static void main(String[] args) {
         DirectorVehiculo directorV = new DirectorVehiculo();
+        DirectorEdificacion directorE = new DirectorEdificacion();
+        DirectorRecurso directorR = new DirectorRecurso();
+        DirectorMilicia directorM = new DirectorMilicia();
         Vehiculo v1 = directorV.getVehiculo("Tanque", 100, 13, 75, 35, 69);
-        System.out.println(v1.getFaseCreacion());
+        Edificacion e1 = directorE.getEdificacion("Centro de Mando", 1000, 0, 0, 0, 0);
+        Recurso r1 = directorR.getRecurso("1", 1000, 1);
+        Milicia m1 = directorM.getMilicia("Soldado", 150, 20, 13, 50, 25, 7);
+        Milicia m2 = directorM.getMilicia("Boki", 330, 25, 15, 100, 75,43,8);
         System.out.println(v1.toString());
+        System.out.println(e1.toString());
+        System.out.println(r1.toString());
+        System.out.println(m1.toString());
+        System.out.println(m2.toString());
     }
 
 }
